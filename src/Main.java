47aff6bb63ac57001b66a2b134bfb4133770d047
Sviat <slsvyatko@gmail.com>
2023-10-17 12:55:37 +0200

    added QuickSort Algorithm

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = new int[1000000];
+        int[] arr = new int[10000000];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = new Random().nextInt(100000);
         }
-        System.out.println(Arrays.toString(MergeSort.mergeSort(arr)));
+        System.out.println(Arrays.toString(QuickSort.quickSort(arr)));
     }
 }
 
